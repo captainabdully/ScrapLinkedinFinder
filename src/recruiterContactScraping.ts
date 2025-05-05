3234511c3246096299cef9ccb2a9b5fa9d7caf66
Ari Gibson <hi@arimgibson.com>
2025-05-04 19:17:23 -0600

    create recruiter scraping script

---
+++ src/recruiterContactScraping.ts
@@ -0,0 +1,80 @@
+import { UnipileClient } from "unipile-node-sdk";
+
+type OutputItem = {
+	fullName: string | null;
+	profileUrl: string | null;
+	emails: string[];
+	phones: string[];
+	currentCompanyRole: string | null;
+};
+
+const { UNIPILE_DSN, UNIPILE_API_KEY, UNIPILE_ACCOUNT_ID } = process.env;
+
+if (!UNIPILE_DSN || !UNIPILE_API_KEY || !UNIPILE_ACCOUNT_ID) {
+	throw new Error("Missing environment variables");
+}
+
+const unipileClient = new UnipileClient(UNIPILE_DSN, UNIPILE_API_KEY);
+
+const recruiterLinkedInURLs: string[] = await Bun.file(
+	"private-data/inputs/recruiterContactScraping.json",
+).json();
+const outputData: OutputItem[] = [];
+
+for (const url of recruiterLinkedInURLs) {
+	const profileUrn = url.split("/in/")[1];
+
+	if (!profileUrn) {
+		console.log(`Invalid LinkedIn URL: ${url}`);
+		continue;
+	}
+
+	try {
+		const contact = await unipileClient.users.getProfile({
+			account_id: UNIPILE_ACCOUNT_ID,
+			identifier: profileUrn,
+			linkedin_sections: ["about", "experience"],
+		});
+
+		if (
+			contact.provider === "LINKEDIN" &&
+			typeof contact.profile_picture_url === "string"
+		) {
+			outputData.push({
+				fullName:
+					contact.first_name && contact.last_name
+						? `${contact.first_name} ${contact.last_name}`
+						: null,
+				profileUrl: contact.public_profile_url || null,
+				emails:
+					contact.contact_info?.emails?.map((email) => email.toLowerCase()) ||
+					[],
+				phones: contact.contact_info?.phones || [],
+				currentCompanyRole:
+					contact.work_experience?.[0]?.company &&
+					contact.work_experience?.[0]?.position
+						? `${contact.work_experience?.[0]?.company} - ${contact.work_experience?.[0]?.position}`
+						: null,
+			});
+			console.log(`Record added for ${url}`);
+		} else {
+			const isLinkedInProvider = contact.provider === "LINKEDIN";
+			console.warn(`Record is not valid for ${url}`, {
+				isLinkedInProvider,
+				hasProfileUrl: isLinkedInProvider
+					? typeof contact?.profile_picture_url === "string"
+					: null,
+			});
+		}
+
+		await Bun.sleep(5000 + Math.random() * 1000);
+	} catch (error) {
+		console.error(`Error fetching profile for ${url}:`, error);
+	}
+}
+
+await Bun.write(
+	`private-data/outputs/recruiterContactScraping-${Date.now()}.json`,
+	JSON.stringify(outputData, null, 2),
+);
+console.log("Done!");
